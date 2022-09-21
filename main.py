331ca5b4d7884efcb27e00da0275d444d8ce4209
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-09-21 12:03:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
-"""
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
-"""
-
+import sys
 from documentcloud.addon import AddOn
+from documentcloud import DocumentCloud
+import subprocess
 
+class GDriveImport(AddOn):
+	def main(self):
+		client = DocumentCloud(#Insert credentials here, redacted for public repository) 
+		file = input("Please provide the link of the Google Drive folder or file you'd like to upload to DocumentCloud \n")
+		cmd = 'python3 gdrivedl.py -P "out" ' + file   
+		
+		try:
+			retcode = subprocess.call(cmd, shell=True)
+			if retcode < 0:
+				print("Child was terminated by signal", -retcode, file=sys.stderr)
+			else:
+				print("Child returned", retcode, file=sys.stderr)
+		except OSError as e:
+			print("Execution failed:", e, file=sys.stderr)
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+		# os.system(cmd)
 
-    def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
-        for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
+		title = input('Please provide a project title for the folder you would like to upload to DocumentCloud \n')
+		project, created = client.projects.get_or_create_by_title(title)
+		obj_list = client.documents.upload_directory('/home/s/gdrivedl/out/')
+		project.document_list = obj_list
+		project.put()
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    GDriveImport().main()
