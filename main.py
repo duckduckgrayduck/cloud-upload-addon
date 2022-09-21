7902faa8c926592a449b3a38d219420189516bc0
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-09-21 13:03:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,7 @@ class GDriveImport(AddOn):
 	def main(self):
 		client = DocumentCloud(#Insert credentials here, redacted for public repository) 
 		file = input("Please provide the link of the Google Drive folder or file you'd like to upload to DocumentCloud \n")
-		cmd = 'python3 gdrivedl.py -P "out" ' + file   
-		
-		try:
-			retcode = subprocess.call(cmd, shell=True)
-			if retcode < 0:
-				print("Child was terminated by signal", -retcode, file=sys.stderr)
-			else:
-				print("Child returned", retcode, file=sys.stderr)
-		except OSError as e:
-			print("Execution failed:", e, file=sys.stderr)
-
+		gdrivedl.main(file)
 		title = input('Please provide a project title for the folder you would like to upload to DocumentCloud \n')
 		project, created = client.projects.get_or_create_by_title(title)
 		obj_list = client.documents.upload_directory('/home/s/gdrivedl/out/')
