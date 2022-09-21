3ad6e85666468d8573a8eac8ee885e9f4f9c835a
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-09-21 13:06:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import subprocess
 
 class GDriveImport(AddOn):
 	def main(self):
-		client = DocumentCloud(#Insert credentials here, redacted for public repository) 
 		file = input("Please provide the link of the Google Drive folder or file you'd like to upload to DocumentCloud \n")
 		gdrivedl.main(file)
 		title = input('Please provide a project title for the folder you would like to upload to DocumentCloud \n')
-		project, created = client.projects.get_or_create_by_title(title)
-		obj_list = client.documents.upload_directory('/home/s/gdrivedl/out/')
+		project, created = self.client.projects.get_or_create_by_title(title)
+		obj_list = self.client.documents.upload_directory('/home/s/gdrivedl/out/')
 		project.document_list = obj_list
 		project.put()
 
