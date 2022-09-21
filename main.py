048ab30eb0a245bf9e2a9291fe744bdc54df43ca
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-09-21 15:56:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from documentcloud.addon import AddOn
 from documentcloud import DocumentCloud
 import gdrivedl	
-
 class GDriveImport(AddOn):
 	def main(self):
-		file = self.data.get("url") #input("URL \n") for local testing
+		file = self.data.get("url") #input("URL \n")
 		gdrivedl.main(file)
-		
+		title = self.data.get("projectname") #input("Title \n") for local testing
 		project, created = self.client.projects.get_or_create_by_title(title)
-		obj_list = self.client.documents.upload_directory('/home/s/gdrivedl/out/')
+		obj_list = self.client.documents.upload_directory('./out/')
 		project.document_list = obj_list
 		project.put()
-
 if __name__ == "__main__":
     GDriveImport().main()
