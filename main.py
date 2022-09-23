d10ab623befa079333e552d42efd9985018edb93
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-09-23 14:16:59 -0500

    Expand functionality to have dropbox,wetransfer, mediafire upload

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from documentcloud import DocumentCloud
 import gdrivedl	
 import shutil
 
-class GDriveImport(AddOn):
+class Import(AddOn):
 	def main(self):
+		os.makedirs(os.path.dirname('./out/'), exist_ok=True)
 		file = self.data.get("url")
-		gdrivedl.main(file)
+		lootdl.grab(file, './out/')
 		title = self.data.get("pname")
 		project, created = self.client.projects.get_or_create_by_title(title)
 		obj_list = self.client.documents.upload_directory('./out/', project=project.id)
 		shutil.rmtree('./out/', ignore_errors=False, onerror=None)
 		
 if __name__ == "__main__":
-    GDriveImport().main()
+	Import().main()
+
